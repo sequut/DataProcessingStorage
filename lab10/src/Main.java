584d0f8bbc1bb0dd9997ccd68cf54aaadaa9d6d4
Andrew <andrewkostin255@ya.ru>
2022-10-13 14:54:14 +0700

    lab10 changed

--- lab10/src/Main.java
+++ lab10/src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     private static final int iterations = 20;
     public volatile static boolean parentOrder;
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Printer printer = new Printer(iterations);
 
         Thread thread = new Thread(printer);
         Object lock = printer.getLock();
 
-
-        Thread.sleep(1000);
         for (int i = 0; i < iterations; i++){
             System.out.println(i + " from main ##");
             synchronized (lock){
